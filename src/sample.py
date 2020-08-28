e8cb09e6f8bc860ec145adcc63850b46731f980a
Shane Mulligan <mullikine@gmail.com>
2020-08-28 17:05:48 +1200

    Fri 28 Aug 2020 17:05:48 NZST

--- src/sample.py
+++ src/sample.py
@@ -74,6 +74,9 @@ def sample_sequence(*, hparams, length, start_token=None, batch_size=None, conte
             logits = next_outputs['logits'][:, -1, :]  / tf.to_float(temperature)
             logits = top_k_logits(logits, k=top_k)
             logits = top_p_logits(logits, p=top_p)
+            # myembed(globals(), locals())
+            logits = tf.transpose(logits)
+            # print(logits.shape)
             samples = tf.multinomial(logits, num_samples=1, output_dtype=tf.int32)
             return [
                 next_outputs['presents'] if past is None else tf.concat([past, next_outputs['presents']], axis=-2),
